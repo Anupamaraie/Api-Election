0bf07cc6a66db2b05a9f0981ee701c468a6765bb
Anupamaraie <anuraie1@gmail.com>
2022-11-18 19:16:20 +0545

    api

--- electionapi/settings.py
+++ electionapi/settings.py
@@ -124,3 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
